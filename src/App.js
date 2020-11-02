c7195b6b6d238ce56f7d3f5ceaaf2a4586ebaf68
ibizaSunrise <milalemekh@gmail.com>
2020-11-02 17:05:01 +0100

    Added pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch,
-  Route } from "react-router-dom";
-  import Home from './pages'
-  import About from './pages/about'
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Services from './pages/services';
+import Contact from './pages/contact';
+import SignUp from './pages/signup';
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Switch>
-        <Route path="/" exact component = {Home}/>
-        <Route path="/about" component = {About}/>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/services' component={Services} />
+        <Route path='/contact-us' component={Contact} />
+        <Route path='/sign-up' component={SignUp} />
       </Switch>
     </Router>
   );
